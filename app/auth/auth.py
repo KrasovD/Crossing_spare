e30f8df63feb6bb23dcc4263da12b6f3a3380af2
Dmitry Karasev <krasov.music@gmail.com>
2023-05-11 21:31:28 +0300

    fix bugs

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -31,8 +31,10 @@ def signup_post():
     password = request.json['password']
     key = request.json['key']
     check = False
+    if key == sha1('Fontanka86'.encode()).hexdigest():
+        check = True
     for user in db.session.query(User).all():
-        if key == sha1(user.username.encode()).hexdigest() or key == sha1('Fontanka86'.encode()).hexdigest():
+        if key == sha1(user.username.encode()).hexdigest():
             check = True
     user = User.query.filter_by(username=username).first()
     if user:
